3298aa02b8fd88a7ff36259a70a792d73722dd34
Adam <ajhfreeth@gmail.com>
2024-11-21 22:09:05 +1100

    initial day 1 part 2 to check for string digits

--- advent2023/day01/main.ml
+++ advent2023/day01/main.ml
@@ -1,15 +1,55 @@
+module StringMap = Map.Make(String)
+
 let read_lines_to_array filename =
   let contents = In_channel.with_open_bin filename In_channel.input_all in
   String.split_on_char '\n' contents
   |> List.filter (fun line -> String.trim line <> "")
   |> Array.of_list
 
+let digit_map =
+  StringMap.empty
+  |> StringMap.add "0" 0
+  |> StringMap.add "1" 1
+  |> StringMap.add "2" 2
+  |> StringMap.add "3" 3
+  |> StringMap.add "4" 4
+  |> StringMap.add "5" 5
+  |> StringMap.add "6" 6
+  |> StringMap.add "7" 7
+  |> StringMap.add "8" 8
+  |> StringMap.add "9" 9
+  |> StringMap.add "zero" 0
+  |> StringMap.add "one" 1
+  |> StringMap.add "two" 2
+  |> StringMap.add "three" 3
+  |> StringMap.add "four" 4
+  |> StringMap.add "five" 5
+  |> StringMap.add "six" 6
+  |> StringMap.add "seven" 7
+  |> StringMap.add "eight" 8
+  |> StringMap.add "nine" 9
+  
+let match_digit_at_pos s i =
+  StringMap.fold
+    (fun key value acc ->
+      let digit_len = String.length key in
+      match acc with
+      | Some a -> Some a
+      | None when (i + digit_len) <= String.length s ->
+        if key == String.sub s i digit_len
+        then Some value
+        else acc
+      | None -> acc)
+    digit_map
+    None
+        
 let find_first_digit s =
   let len = String.length s in
   let rec find_digit i =
     if i >= len then raise (Failure ("Digit not found in " ^ s))
-    else if s.[i] >= '0' && s.[i] <= '9' then int_of_char s.[i] - int_of_char '0'
-    else find_digit (i + 1)
+    else match match_digit_at_pos s i with
+    | Some d -> d
+    | None -> find_digit (i + 1)
   in
   find_digit 0
 
