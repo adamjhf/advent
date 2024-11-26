25398c23fcec7040a06d9df7062df3d92221b639
Adam <ajhfreeth@gmail.com>
2024-11-26 22:20:20 +1100

    day 2 count num possible

--- advent2023/day02/main.ml
+++ advent2023/day02/main.ml
@@ -1,3 +1,5 @@
+module StringMap = Map.Make (String)
+
 let read_lines_to_list filename =
   let contents = In_channel.with_open_bin filename In_channel.input_all in
   String.split_on_char '\n' contents
@@ -18,10 +20,27 @@ let parse_set set =
          | [ count; color ] -> (color, int_of_string count)
          | _ -> failwith "Invalid color count format")
 
+let limits =
+  StringMap.empty
+  |> StringMap.add "red" 12
+  |> StringMap.add "green" 13
+  |> StringMap.add "blue" 14
+
+let is_cube_possible cube =
+  match StringMap.find_opt (fst cube) limits with
+  | Some limit -> snd cube <= limit
+  | None -> failwith ("Unknown color " ^ fst cube)
+
+let is_set_possible set =
+  List.map is_cube_possible set |> List.fold_left ( && ) true
+
+let is_game_possible game =
+  List.map is_set_possible game |> List.fold_left ( && ) true
+
 let () =
   read_lines_to_list "day02/input.txt"
   |> List.map parse_line_to_sets
   |> List.map (List.map parse_set)
-  |> List.map List.length
-  |> List.fold_left ( + ) 0
+  |> List.filter is_game_possible
+  |> List.length
   |> Printf.printf "%d\n"
